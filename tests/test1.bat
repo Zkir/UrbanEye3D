f2ba0077b1bfb9069f492c83cac513b3c81e5750
Kirill "Zkir <mekillot@yandex.ru>
2025-07-25 01:10:09 +0300

    semi-automatic test script

--- tests/test1.bat
+++ tests/test1.bat
@@ -3,6 +3,7 @@ call mvn package
 if ERRORLEVEL 1 goto :err
 xcopy /Y d:\z3dViewer\target\threedviewer-1.0-SNAPSHOT.jar c:\Users\zkir\AppData\Roaming\JOSM\plugins
 java -jar d:\tools\josm\josm-tested.jar https://www.openstreetmap.org/#map=17/55.756311/37.617552
+echo errorlevel %errorlevel%
 goto :end
 :err
 echo test failed
