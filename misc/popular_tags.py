13e2739ff5fa067daf7c0fc8551e9ce911f4a0a1
Kirill B. aka Zkir <mekillot@yandex.ru>
2026-08-05 02:24:24 +0300

    missing popular tags report

--- misc/popular_tags.py
+++ misc/popular_tags.py
@@ -14,7 +14,7 @@ data = response.json()
 
 results = []
 # Вывод первых 200 самых популярных тегов для точек
-for t in data.get("data", [])[:300]:
+for t in data.get("data", [])[:400]:
     key = t['key']
     value = t['value']
     count = t['count_nodes']
@@ -22,13 +22,14 @@ for t in data.get("data", [])[:300]:
 
     if key.startswith("addr:") or key.startswith("source:") or \
        key in ('source', 'created_by', 'place', 'operator', 'operator:wikidata', 'access', 'leaf_cycle', 'level', 'shop', 'opening_hours', 'takeaway', \
-               'hiking', 'wheelchair','fee', 'religion', 'denotation', 'material') or \
+               'hiking', 'wheelchair','fee', 'religion', 'denotation', 'material','tactile_paving','lamp_type','lit','bin') or \
        tag in ('public_transport=stop_position', 'noexit=yes', 'highway=traffic_signals', 'highway=stop', 'highway=give_way') or  key in ('traffic_signals', 'traffic_signals:direction') or\
        tag in ('direction=forward', 'direction=backward', 'bus=yes', 'foot=yes', 'bicycle=yes') or \
        key in ('crossing', 'crossing_ref') or  key.startswith('crossing:') or tag in ('highway=crossing') or \
        key in ('entrance') or \
        tag in ('railway=switch', 'railway=level_crossing') or \
-       tag in ('barrier=kerb', 'kerb=lowered') or \
+       tag in ('barrier=kerb', 'kerb=lowered', 'kerb=flush') or \
+       tag in ('ford=yes') or \
        tag in ('amenity=restaurant', 'amenity=place_of_worship', 'amenity=cafe', 'amenity=school', 'amenity=fast_food', 'amenity=pharmacy', 'amenity=toilets', 'amenity=fuel', 'amenity=bank' ,'amenity=parking' ) or \
        tag in ('natural=peak'):  
         continue
